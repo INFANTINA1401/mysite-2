4ed4f6a553ce5f65b8d7b6333506252ab5a69c78
INFANTINA1401 <147313821+INFANTINA1401@users.noreply.github.com>
2024-11-08 12:45:21 +0530

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -57,4 +57,4 @@ def vote(request, question_id):
 
 # New owner view function
 def owner(request):
-        return HttpResponse("Hello, world. 0a5ebbfe is the polls owner.")
+        return HttpResponse("Hello, world.  0a5ebbfe  is the polls owner.")
